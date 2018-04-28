67480519bdcfeb379129710d0dd2ba346865b2da
serge-sans-paille <serge.guelton@telecom-bretagne.eu>
2018-04-28 15:17:49 +0200

    Python3 support
    + tentative one_loop support for pythran, which compiles correctly but
    two times slower than the unrolled version because the sum on last axis
    cannot be unrolled: dimension is unknown at compile time.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 everything:
-# 	f2py --opt=-O3 -c fort_pot.f90 -m fort_pot
-	f2py2.7 --opt=-O3 -c fort_pot.f90 -m fort_pot
-	pythran naive_pythran_pot.py
+	f2py --opt=-O3 -c fort_pot.f90 -m fort_pot
+#	f2py2.7 --opt=-O3 -c fort_pot.f90 -m fort_pot
+	pythran pythran_pot.py -O3
 
 clean:
 	rm *.o *.mod
--- pythran_pot.py
+++ pythran_pot.py
@@ -13,3 +13,15 @@ def naive_pythran_pot(r,soft):
             dr = np.sqrt(dr+soft2)
             pot += 1./dr
     return pot
+
+# FIXME: pythran does not support float[][3] spec, so it cannot unroll the inner sum
+#pythran export pythran_one_loop_pot(float[][], float)
+def pythran_one_loop_pot(r,soft):
+    N = r.shape[0]
+    pot = 0.
+    soft2 = soft**2
+    for i in range(N-1):
+        dr = np.sqrt(np.sum((r[i,:]-r[i+1:,:])**2,axis=1)+soft2)
+        pot += np.sum(1./dr)
+    return pot
+
